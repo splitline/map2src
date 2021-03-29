5859ca7f13569a16d0f129055c6fcfbed427bb91
ꌗᖘ꒒ꀤ꓄꒒ꀤꈤꍟ <tbsthitw@gmail.com>
2021-03-30 02:14:43 +0800

    Update map2src.py

--- map2src.py
+++ map2src.py
@@ -1,42 +1,65 @@
+#!/usr/bin/env python3
+
 import os
-import os.path
 import errno
 import sys
 import requests
+from urllib.parse import urlparse
 import json
 
-if len(sys.argv) < 2 or len(sys.argv) > 3:
-    print("Usage: python map2src.py url [output_dir]")
-    sys.exit()
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: ./map2src.py http://url/to/bundle.js.map output_dir")
+        sys.exit()
 
-url = sys.argv[1]
-output_dir = sys.argv[2] if len(sys.argv) == 3 else "./"
+    url = sys.argv[1]
+    output_dir = sys.argv[2]
 
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
-if not os.path.isdir(output_dir):
-    print("Error: File `{}` exists, but not a directory".format(output_dir))
-    sys.exit()
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
 
-map_file = json.loads(requests.get(url).text)
+    if not os.path.isdir(output_dir):
+        print("[-] Error: File `{}` exists, but not a directory".format(output_dir))
+        sys.exit()
 
-for i, src_name in enumerate(map_file['sources']):
-    src_name = src_name.replace("webpack:///", "")
-    src_name = os.path.join(output_dir, src_name)
-    try:
-        os.makedirs(os.path.dirname(src_name))
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(os.path.dirname(src_name)):
-            pass
-        else:
-            raise
-    print("[+]", src_name)
-    src = open(src_name, 'w')
-    src.write(map_file['sourcesContent'][i])
-    src.close()
+    if url.startswith('http://') or url.startswith('https://'):
+        json_str = requests.get(url, verify=False).text
+    else:
+        json_str = open(url).read()
 
-map_filename = os.path.join(output_dir, os.path.basename(url))
-print("[+]", map_filename)
-json.dump(map_file, open(map_filename, "w"))
+    print("[*] Source map loaded.")
 
-print("Done.")
+    map_file = json.loads(json_str)
+
+    if len(map_file['sourcesContent']) != len(map_file['sources']):
+        print("[*] Warning: Length of `sources` and `sourcesContent` doesn't match")
+
+    for i, orig_src_path in enumerate(map_file['sources']):
+        orig_src_path = os.path.abspath(urlparse(orig_src_path).path).lstrip("/")
+        src_path = os.path.join(output_dir, orig_src_path)
+
+        if os.path.exists(src_path):
+            print(f"[*] {src_path} exists, skipped.")
+            continue
+
+        try:
+            os.makedirs(os.path.dirname(src_path))
+        except OSError as err:
+            if err.errno == errno.EEXIST and os.path.isdir(os.path.dirname(src_path)):
+                pass
+            else:
+                raise err
+        print("[+] Saveing", src_path)
+
+        try:
+            src = open(src_path, 'w')
+            src.write(map_file['sourcesContent'][i])
+            src.close()
+        except:
+            print("[-] Failed:", src_path)
+
+    map_filename = os.path.join(output_dir, os.path.basename(url))
+    print("[+]", map_filename)
+    json.dump(map_file, open(map_filename, "w"))
+
+    print("Done.")
